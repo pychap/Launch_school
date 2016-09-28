cde7c011d65bc439be10cdb1fe01894d4ba11638
pychap <pychapster@gmail.com>
2016-09-27 20:49:31 -0700

    Calculator lesson fine

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -13,4 +13,19 @@ num1 = Kernel.gets().chomp()
 Kernel.puts("What's the second number?")
 num2 = Kernel.gets().chomp()
 
-Kernel.puts(num1 + " " + num2) 
+# Kernel.puts(num1 + " " + num2)
+
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = num1.to_i() + num2.to_i()
+elsif operator == '2'
+  result = num1.to_i() - num2.to_i()
+elsif operator == '3'
+  result = num1.to_i() * num2.to_i()
+else operator == '4'
+  result = num1.to_f() / num2.to_f()
+end
+
+Kernel.puts("The result is #{result}")
