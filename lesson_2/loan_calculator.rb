ca1cbc785f6e628f226c611ee5f9fa63e0022475
pychap <pychapster@gmail.com>
2016-10-03 20:16:50 -0700

    Monday's work on loan calculator

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mort_apr_calc.rb')
 # greet
 # ask for loan amount, APR, loan duration in months
 # process the information
@@ -7,3 +9,22 @@
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
+
+def valid_number?(num)
+  # if valid_number is a letter will produce 0
+  num.to_i().nonzero?
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
+end
+
+prompt[MESSAGES("Welcome")]
