f344a0d77cbbf967c863ceef8b615196dc5a4225
pychap <pychapster@gmail.com>
2016-10-06 16:52:01 -0700

    Resolving file conflicts

--- exercises_scratch.rb
+++ exercises_scratch.rb
@@ -1,9 +1,11 @@
-
+numbers = []
 
 loop do
-  number = rand(100)
-    puts number
-    if number.between?(0, 10)
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  numbers << input
+  if numbers.size >= 5
     break
   end
 end
+p numbers
