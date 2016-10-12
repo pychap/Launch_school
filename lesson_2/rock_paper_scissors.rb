642796a9ca2686d1bc8ce2bfd097523d0e9dafd0
pychap <pychapster@gmail.com>
2016-10-12 13:53:02 -0700

    Updating RCP program

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,19 @@
-VALID_CHOICES = ['rock', 'scissors', 'paper']
+VALID_CHOICES = %w(rock scissors paper)
 
 def prompt(message)
   Kernel.puts(">= #{message}")
 end
 
+def win?(first, second)
+   (first == 'rock' && second == 'scissors') ||
+   (first == 'paper' && second == 'rock') ||
+   (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') || (player == 'paper' && computer == 'rock') || (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') || (player == 'paper' && computer == 'scissors') || (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
@@ -16,11 +22,10 @@ end
 
 # main loop
 loop do
-choice = ''
+  choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
-
     if VALID_CHOICES.include?(choice)
       break
     else
