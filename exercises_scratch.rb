48c78d8980b325dceee24ebd566153278355bba8
Peter Chapman <pychapster@gmail.com>
2016-10-17 12:47:21 -0700

    merging before pull

--- exercises_scratch.rb
+++ exercises_scratch.rb
@@ -1,20 +1,4 @@
-def valid_number?(number_string)
-  number_string.to_i.to_s == number_string && number_string.to_i != 0
-end
+pets = ['cat', 'dog', 'fish', 'lizard']
 
-def read_number
-  loop do
-    puts '>> Please enter a positive or neg number.'
-    number = gets.chomp
-    return number.to_i if valid_number(number)
-    puts '>> Invalid input. Only non-zero integers are allowed.'
-  end
-end
-
-integer_one = nil
-integer_two = nil
-
-loop do
-  first_number = read_number
-  second_number = read_number
-end
+my_pets = pets[2..3]
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
