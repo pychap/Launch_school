010302388872268d40b06549aa0beb00c6e9a320
Peter Chapman <pychapster@gmail.com>
2016-10-20 15:03:02 -0700

    Readying for repo pull

--- exercises_scratch.rb
+++ exercises_scratch.rb
@@ -1,4 +1,4 @@
 pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[1..2]
 
-my_pets = pets[2..3]
-puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+puts "I have a pet #{my_pets[1]} and a pet #{my_pets[0]}"
