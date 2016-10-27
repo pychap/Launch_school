f7359ea87706e5d4afcde1e5d49ef5143fde45ca
pychap <pychapster@gmail.com>
2016-10-26 22:28:43 -0700

    Beginning of RSPB program

--- lesson_2/rock_paper_scissors_bonus.rb
+++ lesson_2/rock_paper_scissors_bonus.rb
@@ -1,30 +1,29 @@
 # https://github.com/haopingfan/exercise/blob/master/101/Lesson_2/rps_bonus/revised_rock_paper_scissors_bonus.rb
-
+require 'pry'
 VALID_CHOICES = %w(rock scissors paper lizzard spock)
+
+computer_score = 0
+player_score = 0
+choice = ''
 
 def integer?(string)
   /^\d+$/.match(string)
+end
+
+def clear_screen
+  system('clear') || system('cls')
 end
 
 def prompt(message)
   Kernel.puts(">= #{message}")
 end
 
-def ask_user_name
-  user_name = ''
-  prompt "Please enter your name:"
-  loop do
-    user_name = gets.chomp
-    break if user_name =~ /^[A-Za-z]+$/
-    prompt "Please enter a valid name:"
-  end
-  user_name
-end
-
 def win?(first, second)
-   (first == 'rock' && second == 'scissors') ||
-   (first == 'paper' && second == 'rock') ||
-   (first == 'scissors' && second == 'paper')
+    (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
+      (first == 'paper' && (second == 'rock' || second == 'spock')) ||
+      (first == 'scissors' && (second == 'paper' || second == 'rock')) ||
+      (first == 'spock' && (second == 'scissors' || second == 'rock')) ||
+      (first == 'lizard' && (second == 'spock' || second == 'paper'))
 end
 
 def display_results(player, computer)
@@ -37,9 +36,22 @@ def display_results(player, computer)
   end
 end
 
-# main loop
+clear_screen
+
+prompt("Welcome, what's your name?")
+user_name = ''
 loop do
-  choice = ''
+  user_name = gets.chomp
+  break if user_name =~ /^[A-Za-z]+$/
+  prompt "Please enter a valid name:"
+end
+
+prompt("Hi #{user_name}, welcome.")
+prompt("Whoever makes it to 5 points first wins!")
+
+# main loop
+# score record in this loop
+loop do
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
@@ -56,9 +68,22 @@ loop do
 
   display_results(choice, computer_choice)
 
+  # score counter goes here
+  # add_up_scores(choice, computer_choice)
+  if win?(choice, computer_choice)
+    player_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end
+
+  prompt("Score: you #{player_score}, computer #{computer_score}.")
+  prompt("First to score 5 wins!")
+  break if player_score == 5 || computer_score == 5
+
   prompt("Would you like to play again? (y or n)")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
+  clear_screen
 end
 
-prompt("Thank you for playing.")
+prompt("Thank you for playing #{user_name}.")
