f752885df84d6b4c53f92a28136e5e66b9de46cc
pychap <pychapster@gmail.com>
2016-10-30 20:36:10 -0700

    Latest

--- exercises_scratch.rb
+++ exercises_scratch.rb
@@ -1,10 +1,11 @@
+require 'pry'
 def factors(number)
   dividend = number
   divisors = []
-  begin
+  while dividend > 0 
     divisors << number / dividend if number % dividend == 0
     dividend -= 1
-  end until dividend == 0
+    end
   divisors
 end
 
