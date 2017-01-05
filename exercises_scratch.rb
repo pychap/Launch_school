849e707d74db4991ba1ab22cbdaae1ebdf4b2095
pychap <pychapster@gmail.com>
2017-01-04 19:11:47 -0800

    Making scratch file dissapear.

--- exercises_scratch.rb
+++ exercises_scratch.rb
@@ -1,10 +1,23 @@
 require 'pry'                  # => true
 require 'seeing_is_believing'  # => true
 
-def create_hash
-  new_board = {}
-  (1..9).each {|val| new_board[val] = ' '}
-    new_board
+sentence_of_words = "Sparky the cat sat on the mat with the hat."
+
+def words_from_string(string)
+  string.downcase.scan(/[\w']+/)
 end
 
-p create_hash
+array_of_words = words_from_string(sentence_of_words)
+# binding.pry
+
+def count_frequency(word_list)
+  counts = Hash.new(0)
+  for word in word_list
+    counts[word] += 1
+    binding.pry
+  end
+  counts
+  # binding.pry
+end
+
+p count_frequency(array_of_words)
