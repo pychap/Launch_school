528b4985f531cfc6bd22f82248cd3153a9ead40d
pychap <pychapster@gmail.com>
2017-01-05 21:24:56 -0800

    Scratch file in my quest to create/add to a hash.

--- lesson_2/ex_scratch.rb
+++ lesson_2/ex_scratch.rb
@@ -1,39 +1,5 @@
 require 'pry'
 
-def valid_number?(number_string)
-  number_string.to_i.to_s == number_string && number_string.to_i != 0
-end
+array = ["apples", "bananas", "coconuts", "watermelons"]
 
-integer_one = ''
-integer_two = ''
-sum = nil
-
-loop do
-  loop do
-  	puts "Enter a positive or negative integer."
-  	integer_one = gets.chomp	
-  	break if valid_number?(integer_one)
-    puts "Invalid input. Only non-zero integers are allowed."
-  end  
-
-  loop do
-  	puts "Enter the next positive or negative integer, opposite of the 1st."
-  	integer_two = gets.chomp	
-  	break if valid_number?(integer_two)
-    puts "Invalid input. Only non-zero integers are allowed."
-  end
-  # binding.pry
-  
-  if (integer_one.to_i <=> integer_two.to_i) <= 0
-  	puts "Please enter one positive and one negative number."
-  else
-  	break
-  end
-end  
-
-sum = integer_one.to_i + integer_two.to_i
-puts sum
-
-
-
-
+hash = array.inject({}) { |h,fruit| h.merge(fruit => f(fruit)) }
