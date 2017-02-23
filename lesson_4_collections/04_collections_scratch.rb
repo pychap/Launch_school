9eb818156671afac411ff0baf8168de0e001c178
Peter Chapman <pychapster@gmail.com>
2017-02-22 17:49:14 -0800

    Adding latest attempt at ex 16

--- lesson_4_collections/04_collections_scratch.rb
+++ lesson_4_collections/04_collections_scratch.rb
@@ -1,33 +1,3 @@
 require 'pry'
 
-[[[1, 2], [3, 4]], [5, 6]].map do |arr|
-  arr.map do |el|
-    binding.pry
-    if el.to_s.size == 1    # it's an integer
-      el + 1
-    else                    # it's an array
-      el.map do |n|
-        n + 1
-      end
-    end
-  end
-end
-
-munsters = {
-  "Herman" => { "age" => 32, "gender" => "male" },
-  "Lily" => { "age" => 30, "gender" => "female" },
-  "Grandpa" => { "age" => 402, "gender" => "male" },
-  "Eddie" => { "age" => 10, "gender" => "male" }
-}
-total_male_ages = 0
-
-munsters.each do |name, details|
-  case 
-  when details["gender"] == "male"
-    total_male_ages += details["age"]
-  end
-  total_male_ages
-end
-
-p total_male_ages
-
+["b", 5, 3, 1, 4, 2, 7, 1, 2, 5, 3, 8, "b", 1, 7, "e"].each 
--- lesson_5/ex_16.rb
+++ lesson_5/ex_16.rb
@@ -1,22 +1,25 @@
 require 'pry'
 
 def uuid_creator
-  letters = ('a'...'f').to_a
-  nums = (1...9).to_a
+  letters = ('a'..'f').to_a
+  nums = (1..9).to_a
   base_set = letters.concat(nums)
   build_uuid = []
   sections = [8, 4, 4, 4, 12]
   new_uuid = ''
 
   sections.each_with_index do |item, idx|
-    while item > 0
+    item.times do
      build_uuid << base_set.sample
-     # binding.pry
-      item -= 1
     end
-    new_uuid << build_uuid.join + "-"
-    binding.pry
+    # binding.pry
+    if idx < idx.size
+      new_uuid += build_uuid.join + "-"
+  else
+      new_uuid += build_uuid.join
+    end
   end
+  new_uuid
 end
 
-uuid_creator
+p uuid_creator
