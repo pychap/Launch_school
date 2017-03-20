7f523047419438f3822e1053db276c0140ed99e3
Peter Chapman <pychapster@gmail.com>
2017-03-20 08:28:55 -0700

    Final, final updates to repo

--- lesson_6/6_scratch.rb
+++ lesson_6/6_scratch.rb
@@ -1,16 +1,29 @@
 require 'pry'
 
 
-puts "Play again? Y or N"
-  play_again = gets.chomp
-  # if play_again != play_again.downcase('y') || play_again.downcase('n')
-  #   puts "Please choose either Y or N"
 
-  case play_again
-  when 'n'
-    break
-  when 'y'
-    puts "Play on!"
-  else
-    puts "Please choose either Y or N"
-  end
+munsters = {
+  "Herman" =>  { "age" => 32, "gender" => "male" },
+  "Lily" =>    { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" =>   { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+
+def amethod(param)
+	param += " universe"
+	param.object_id
+	binding.pry
+	# param + " universe"
+	# str.object_id
+	param << " world"
+end
+
+str = "hello"
+amethod(str)
+
+str.object_id
+binding.pry
+
+puts str
